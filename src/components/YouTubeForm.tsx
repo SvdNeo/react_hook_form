db223301ccddfecd6ceee4b9cf66eefb98f28f0f
SvdNeo <sanjay.d@neoito.com>
2024-02-07 16:20:56 +0530

    custom validation

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -9,23 +9,55 @@ type formValues = {
 
 export const YouTubeForm = () => {
   const form = useForm<formValues>();
-  const { register, control, handleSubmit,formState } = form;
-  const {errors} = formState;
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
   const onSubmit = (data: formValues) => {
     console.log(data);
   };
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        <label htmlFor="username">UserName</label>
-        <input type="text" id="username" {...register("username",{required:{value:true,message:"username is required"}})} />
-        <p className="error">{errors.username?.message}</p>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" {...register("email",{pattern:{value:/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,"message":"invalid email"}})} />
-        <p className="error">{errors.email?.message}</p>
-        <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" {...register("channel",{required:{value:true,message:"channel name is required"}})} />
-        <p className="error">{errors.channel?.message}</p>
+        <div className="form-control">
+          <label htmlFor="username">UserName</label>
+          <input
+            type="text"
+            id="username"
+            {...register("username", {
+              required: { value: true, message: "username is required" },
+            })}
+          />
+          <p className="error">{errors.username?.message}</p>
+        </div>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            {...register("email", {
+              pattern: {
+                value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                message: "invalid email",
+              },
+              validate: (fieldValue) => {
+                return (
+                  fieldValue !== "admin@example.com" || "Enter different mail id"
+                );
+              },
+            })}
+          />
+          <p className="error">{errors.email?.message}</p>
+        </div>
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input
+            type="text"
+            id="channel"
+            {...register("channel", {
+              required: { value: true, message: "channel name is required" },
+            })}
+          />
+          <p className="error">{errors.channel?.message}</p>
+        </div>
         <button type="submit">Submit</button>
       </form>
       <DevTool control={control} />
