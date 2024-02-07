ac3bacefa81a6a82b5487e0a821a8d6727e577d8
SvdNeo <sanjay.d@neoito.com>
2024-02-07 13:24:17 +0530

    created on submit function and passed it to form element through argument of handlesubmit prop

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,13 +1,18 @@
 
 import {useForm}   from "react-hook-form";
 import {DevTool} from '@hookform/devtools'
+type formValues = {
+  username: string,
+  email:string, 
+  channel:string
+}
 export const YouTubeForm = () => {
-  const form = useForm();
-  const {register,control} = form;
-
+  const form = useForm<formValues>();
+  const {register,control,handleSubmit} = form;
+const onSubmit = (data:formValues) => {console.log(data)}
   return (
     <div>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <label htmlFor='username'>UserName</label>
             <input type='text'  id='username' {...register('username')} />
             <label htmlFor='email'>Email</label>
